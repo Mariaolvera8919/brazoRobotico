c85cb0161618702f7a957d4a02bc7b22f6e554fc
Mariaolvera8919 <32545926+Mariaolvera8919@users.noreply.github.com>
2018-05-15 22:36:23 -0500

    Add files via upload

---
+++ ProbandoServo.ino
@@ -0,0 +1,49 @@
+
+ 
+// Incluímos la librería para poder controlar el servo
+#include <Servo.h>
+ 
+// Declaramos la variable para controlar el servo
+Servo servoMotor;
+ 
+void setup() {
+  // Iniciamos el monitor serie para mostrar el resultado
+  Serial.begin(9600);
+ 
+  // Iniciamos el servo para que empiece a trabajar con el pin 9
+  servoMotor.attach(9);
+
+  // Inicializamos al ángulo 0 el servomotor
+  servoMotor.write(0);
+}
+ 
+void loop() {
+  for(int i = 0; i <= 100; i += 20) {
+    servoMotor.write(i);
+    delay(1000);
+
+    Serial.println(servoMotor.read());
+  }
+
+  for(int i = 100; i >= 0; i -= 20) {
+    servoMotor.write(i);
+    delay(1000);
+
+    Serial.println(servoMotor.read());
+  }
+
+  /*servoMotor.write(0);
+  delay(1000);
+  
+  servoMotor.write(90);
+  delay(1000);
+
+  servoMotor.write(180);
+  delay(1000);
+
+  servoMotor.write(270);
+  delay(1000);
+
+  servoMotor.write(360);
+  delay(1000);*/
+}
