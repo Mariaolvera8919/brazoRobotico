a40952344a1a927724949ff2d3c86a019340a677
HughRod <36654325+HughRod@users.noreply.github.com>
2018-05-27 01:25:31 -0500

    Adición de comentarios al código.

--- TestBrazo.java
+++ TestBrazo.java
@@ -1,3 +1,5 @@
+/*PROGRAMA EN JAVA QUE PERMITE CONTROLAR UN BRAZO ROBÓTICO
+ * ENVIANDO DATOS AL ARDUINO PARA MANIPULAR AL MISMO.*/
 package BrazoRobótico;
 
 import java.awt.BorderLayout;
@@ -8,13 +10,13 @@ public class TestBrazo {
 
 	public static void main(String[] args) {
 		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"); // ESTABLECER EL LOOK AND FEEL NIMBUS
 		} catch (Exception e) {}
 		
-		JFrame frame = new JFrame("Control Brazo Robótico");
+		JFrame frame = new JFrame("Control Brazo Robótico");			// CREAR OBJETO JFRAME
 		
 		frame.setLayout(new BorderLayout()); 					// ESTABLECER LAYOUT 
-		frame.setSize(900, 600); 						// ESTABLECER TAMAÑO DE LA VENTANA
+		frame.setSize(1200, 600); 						// ESTABLECER TAMAÑO DE LA VENTANA
 		frame.setLocationRelativeTo(null); 					// ESTABLECER VENTANA AL CENTRO DE LA PANTALLA
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 			// TERMINAR PROGRAMA AL CERRAR VENTANA
 		frame.add(new Contenedora()); 						// AGREGAR PANEL AL JFRAME
